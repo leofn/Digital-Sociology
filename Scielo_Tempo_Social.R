da36d0c8cb51c582be2e649d2982a0a333385972
Leonardo F. Nascimento <leofn3@gmail.com>
2016-04-29 22:40:04 -0300

    Added files via upload

---
+++ Scielo_Tempo_Social.R
@@ -0,0 +1,30 @@
+rm(list=ls())
+options(warn=-1)
+options(show.error.messages = T)
+library(XML)
+
+dados <- data.frame()
+url.scielo <- "http://www.scielo.br/scielo.php?script=sci_issues&pid=0103-2070&lng=en&nrm=iso"
+pagina <- xmlRoot(htmlParse(readLines(url.scielo)))
+links <- getNodeSet(pagina,"//font/a")
+links <- xmlSApply(links, xmlGetAttr, name = "href")
+dados <- cbind(links) 
+##sempre verificar o subconjunto do data.frame abaixo para saber qual subconjunto deve pegar
+links <- dados[3:52,] 
+
+dados.pdf <- data.frame()
+for (i in links){
+  print(i)
+  links.pdf <- xmlRoot(htmlParse(readLines(i)))
+  links.pdf <- getNodeSet(links.pdf, "//a") 
+  links.pdf <- xmlSApply(links.pdf, xmlGetAttr, name = "href")
+  dados.pdf <- rbind(dados.pdf, cbind(links.pdf))
+} 
+##atentar para o parâmetro do Grep "/pdf/xxx" o XXX deverá ser modificado de acordo
+##com o nomeda revista
+lista.links.final <- dados.pdf[grep("/pdf/ts/",dados.pdf$links.pdf),]
+
+lista.links.final <- data.frame(sapply(lista.links.final, as.character), stringsAsFactors=FALSE)
+frango <- "http://www.scielo.br/" ## "frango" é uma homenagem ao Leo Barone do MQ-FAFICH
+testefinal <- with(lista.links.final, paste(frango,lista.links.final$sapply.lista.links.final..as.character. , sep=""))
+write.csv(testefinal, "lista.ts.csv") #lista de links para download da revista

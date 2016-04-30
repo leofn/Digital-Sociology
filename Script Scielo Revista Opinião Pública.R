7a4794d8e45b7c52ad3cf11e9033c6f6de5be947
Leonardo F. Nascimento <leofn3@gmail.com>
2016-04-30 13:30:24 -0300

    Update Script Scielo Revista Opinião Pública.R

--- Script Scielo Revista Opinião Pública.R
+++ Script Scielo Revista Opinião Pública.R
@@ -1,6 +1,7 @@
 ##########################################################################################################
-### Script para extração de todos os pdfs da Revista Opinião Pública da base Scielo
+### Script para extração de todos os pdfs da Revista Tempo Social da base Scielo
 ### Autor: Leonardo F. Nascimento
+### Carthago delenda est: big data social science is the future 
 ##########################################################################################################
 rm(list=ls())
 options(warn=-1)
@@ -24,8 +25,7 @@ for (i in links){
   links.pdf <- xmlSApply(links.pdf, xmlGetAttr, name = "href")
   dados.pdf <- rbind(dados.pdf, cbind(links.pdf))
 } 
-##atentar para o parâmetro do Grep "/pdf/xxx" o XXX deverá ser modificado de acordo
-
+##atentar para o parâmetro do Grep "/pdf/xx" o XX deverá ser modificado de acordo com a sigla da revista no Scielo
 lista.links.final <- dados.pdf[grep("/pdf/op", dados.pdf$links.pdf),]
 lista.links.final <- data.frame(sapply(lista.links.final, as.character), stringsAsFactors=FALSE)
 frango <- "http://www.scielo.br" ## "frango" é uma homenagem ao Leo Barone do MQ-FAFICH
@@ -35,5 +35,5 @@ for (url in listafinal) {
   newName <- paste (format(Sys.time(), "%Y%m%d%H%M%S"), "-", basename(url), sep =" ")
   download.file(url, destfile = newName)
 }
-
+### FIM
 

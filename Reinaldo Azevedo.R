fbaf0e25e61856d4c7e87faf7714c83a49f06c2e
Leonardo F. Nascimento <leofn3@gmail.com>
2016-05-01 15:24:47 -0300

    Added files via upload

---
+++ Reinaldo Azevedo.R
@@ -0,0 +1,41 @@
+rm(list=ls())
+options(warn=-1)
+library("RCurl")
+library("XML")
+baseurl <- "http://veja.abril.com.br/blog/reinaldo/pagina/XX/"
+dados <- data.frame()
+
+for (i in 1:50){
+  print(i)
+  url <- gsub("XX", i, baseurl)
+  url <- xmlRoot(htmlParse(readLines(url)))
+  ## pegar os links
+  links <- getNodeSet(url, "//span/h3/a") 
+  links <- xmlSApply(links, xmlGetAttr, name = "href")
+  ## montar o dataframe
+  dados <- rbind(dados, cbind(links))
+  }
+  ### pegar o conteúdo das matérias
+  dados.final <- data.frame()
+  for (j in links){
+  print(j)
+  url <- xmlRoot(htmlParse(readLines(j)))
+  ## pegar a data
+  data <- getNodeSet(url, "//p/time") 
+  data <- xmlSApply(data, xmlValue)
+  ## pegar o título
+    titulo <- getNodeSet(url, "//header/div[1]/h1") 
+  titulo <- xmlSApply(titulo, xmlValue)
+  ## pegar o texto
+  texto <- getNodeSet(url, "//article/div/div/p") 
+  texto <- xmlSApply(texto, xmlValue)
+  texto <- paste(texto, collapse = "")
+  ## montar o dataframe final com os dados
+  dados.final <- rbind(dados.final, cbind(data, titulo, texto))
+  }
+### Gerar um .rtf com uma tabela onde consta Data, Título e Texto
+library(rtf)
+rtffile <- RTF("azevedo.rtf")  # pode ser um .rtf ou um .doc
+addTable(rtffile, dados.final)
+done(rtffile)
+

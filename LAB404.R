ca2c6f1b8faf2729f325e2a21e5f5e261db66cc4
Leonardo F. Nascimento <leofn3@gmail.com>
2018-04-23 15:35:37 -0300

    Add files via upload

---
+++ LAB404.R
@@ -0,0 +1,144 @@
+####################################################################################
+#                                                                                  #
+#                   LAB404 - Experimentos de Webscraping e Análise                 #
+#                                                                                  #    
+#3#3#3#3#3#3#3#3#3#3#3#3#3#3#3#3#3#3#3#3#3#3#3#3#3#3#3#3#3#3#3#3#3#3#3#3#3#3#3#3#3#3
+
+rm(list=ls())
+options(warn=-1)
+options(show.error.messages = T)
+library(XML)
+library(magrittr)
+
+baseurl <- "http://www.lab404.ufba.br/?paged=xxx"
+
+dados <- data.frame()
+
+for (i in 1:25){
+  print(i)
+  url <- gsub("xxx", i, baseurl)
+  url.conteudo <- url %>% readLines %>% htmlParse %>% xmlRoot
+  ## Titulos das Postagens
+  titulos <- getNodeSet(url.conteudo, "//h2/a")
+  titulos <- xmlSApply(titulos, xmlValue)
+  ## Autores das Postagens
+  autores <- getNodeSet(url.conteudo, "//span/span/a")
+  autores <- xmlSApply(autores, xmlValue)
+  ## Datas das Postagens
+  data <- getNodeSet(url.conteudo, "//a/time")
+  data <- xmlSApply(data, xmlGetAttr, name = "datetime")
+  ## Links das Postagens
+  links.dos.titulos <- getNodeSet(url.conteudo, "//h2/a")
+  links.dos.titulos <- xmlSApply(links.dos.titulos, xmlGetAttr, name = "href")
+  dados <- rbind(dados, cbind(titulos, autores, data,links.dos.titulos))
+}
+#####
+
+#Se quiser, pode ser criada um planilha com uma parte do conteúdo do blog.
+write.csv(dados, "lab404_titulo_autores_data_links.csv")
+####
+
+## Etapa 2: pegar o conteúdos dos post
+## Primeira coisa: criar um objetos com todos os links que estão em dados
+
+links.posts <- dados$links.dos.titulos
+
+dados.posts <- data.frame()
+
+for (url in links.posts){
+  print(url)
+  textos.posts <- xmlRoot(htmlParse(readLines(url)))
+  textos.posts <- getNodeSet(textos.posts, "//article/div/div")
+  textos.posts <- xmlSApply(textos.posts, xmlValue)
+  dados.posts <- rbind(dados.posts, cbind(textos.posts))
+}
+
+write.table(dados.posts$, "posts-lab404.csv")
+dates <- as.Date(dados$data)
+
+#### Histograma de Posts
+
+hist(dates, "years", 
+     axis = T, 
+     main = "Frequência de Postagens no site LAB404 por ano (2009-2018)", 
+     xlab = "Ano",
+     ylab = "frequências absolutas",
+     plot = T,
+     ylim = 0,30,by=10)
+
+
+
+
+### Analisando os dados
+
+#install.packages("plyr")
+#install.packages("tm")  # for text mining
+#install.package("SnowballC") # for text stemming
+#install.packages("wordcloud") # word-cloud generator 
+#install.packages("RColorBrewer") # color palettes
+
+library(plyr)
+x = count(dados$autores, c())
+x <- x[order(x$freq),] 
+barplot(x$freq, names.arg = x$x, col = "blue", main = "Número de posts por autor no blog LAB404.COM")
+text(x= values+4,labels=as.character(values),  pos=1, offset=.7, xpd=TRUE, col = "red")
+
+
+#hist(x$freq, col=heat.colors(max_num), breaks=max_num, 
+#     xlim=c(0,max_num), right=F, main="Número de posts por autor no blog LAB404.COM", las=1)
+Testando
+
+# Load
+library("tm")
+library("SnowballC")
+library("wordcloud")
+library("RColorBrewer")
+
+# Load the data as a corpus
+docs <- Corpus(VectorSource(dados.posts$textos.posts))
+toSpace <- content_transformer(function (x , pattern ) gsub(pattern, " ", x))
+docs <- tm_map(docs, toSpace, "/")
+docs <- tm_map(docs, toSpace, "@")
+docs <- tm_map(docs, toSpace, "\\|")
+# Convert the text to lower case
+docs <- tm_map(docs, content_transformer(tolower))
+# Remove numbers
+docs <- tm_map(docs, removeNumbers)
+# Remove english common stopwords
+docs <- tm_map(docs, removeWords, stopwords(kind = "pt"), stopwords(kind = "en"))
+# Remove your own stop word
+# specify your stopwords as a character vector
+docs <- tm_map(docs, removeWords, c("mas","por","como","dos","das","para","and","com","uma","the","que","lana","ncia","lab", "semana", "links", "capítulo","sobre", "coisas", "resenha")) 
+# Remove punctuations
+docs <- tm_map(docs, removePunctuation)
+# Eliminate extra white spaces
+docs <- tm_map(docs, stripWhitespace)
+# Text stemming
+# docs <- tm_map(docs, stemDocument)
+dtm <- TermDocumentMatrix(docs)
+m <- as.matrix(dtm)
+v <- sort(rowSums(m),decreasing=TRUE)
+d <- data.frame(word = names(v),freq=v)
+head(d, 10)
+set.seed(1234)
+wordcloud(words = d$word, freq = d$freq, min.freq = 1,
+          max.words=200, random.order=FALSE, rot.per=0.35, 
+          colors=brewer.pal(8, "Dark2"))
+
+#########################
+findFreqTerms(dtm, lowfreq = 4)
+findAssocs(dtm, terms = "pesquisa", corlimit = 0.3)
+barplot(d[1:10,]$freq, las = 2, names.arg = d[1:10,]$word,
+        col ="lightblue", main ="Palavras mais Frequentes",
+        ylab = "Frequência de Palavras")
+
+
+### Criar o csv dos data.frames
+write.csv(dados, "dados-lab404.csv")
+### Há valores em branco nos posts que eu gostaria de eliminar 
+###  mas ainda não consegui
+
+##### Frenquecia dos Posts
+x <- as.Date(dados$data)
+basic.histogram <- hist(x,main="Frequência de postagens do Site Lab404.net", breaks = "year",col="grey", plot = T) 
+
